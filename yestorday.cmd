bd22bf2b71f40f5ae9a4b961baf7614b4a3f8f78
鄭世梧 <45@cht.com.tw>
2020-10-08 15:32:01 +0800

    時間修正

--- yestorday.cmd
+++ yestorday.cmd
@@ -1,12 +1,25 @@
 @echo off
+
 set day=-1
 echo >"%temp%\%~n0.vbs" s=DateAdd("d",%day%,now) : d=weekday(s)
 echo>>"%temp%\%~n0.vbs" WScript.Echo year(s)^& right(100+month(s),2)^& right(100+day(s),2)
-for /f %%a in ('cscript /nologo "%temp%\%~n0.vbs"') do set "result=%%a"
+for /f %%a in ('cscript /nologo "%temp%\%~n0.vbs"') do set "yestorday=%%a"
 del "%temp%\%~n0.vbs"
-set "YYYY=%result:~0,4%"
-set "MM=%result:~4,2%"
-set "DD=%result:~6,2%"
-set "result=%yyyy%-%mm%-%dd%"
-echo %result%
+set "YYYY=%yestorday:~0,4%"
+set "MM=%yestorday:~4,2%"
+set "DD=%yestorday:~6,2%"
+set "yestorday=File_%yyyy%%mm%%dd%"
+echo %yestorday%
+
+set day=-3
+echo >"%temp%\%~n0.vbs" s=DateAdd("d",%day%,now) : d=weekday(s)
+echo>>"%temp%\%~n0.vbs" WScript.Echo year(s)^& right(100+month(s),2)^& right(100+day(s),2)
+for /f %%a in ('cscript /nologo "%temp%\%~n0.vbs"') do set "threeDaysAgos=%%a"
+del "%temp%\%~n0.vbs"
+set "YYYY=%threeDaysAgos:~0,4%"
+set "MM=%threeDaysAgos:~4,2%"
+set "DD=%threeDaysAgos:~6,2%"
+set "threeDaysAgos=File_%yyyy%%mm%%dd%"
+echo %threeDaysAgos%
+
 pause
